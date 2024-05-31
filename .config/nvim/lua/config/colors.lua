6f0bd81ed44b4e1938a9db7488f11510c00aa3b8
Vincent Whizin <vincent@wheezy.ftp.sh>
2024-05-31 14:02:18 -0700

    nvim: Use nvim's "default" colorscheme
    Neovim 0.10.0 changes default colorscheme. The old default is now called
    "vim". See release notes -- there seems to be an update with the
    coloring the new default, not sure if there's any lost functionality
    using the old default.

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -3,6 +3,7 @@ vim.opt.termguicolors = true
 vim.opt.encoding = 'utf-8'
 vim.opt.gfn = 'Source Code Pro 12'
 
+vim.cmd.colorscheme('vim')
 vim.opt.background = 'dark'
 vim.cmd.highlight({ 'Pmenu', 'guibg=Black', 'guifg=White' })
 vim.cmd.highlight({ 'ColorColumn', 'guibg=Gray' })
